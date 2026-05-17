c7d5dd90d1eb64641afa4bc4d6f868dbc535c75b
Cindy <0852574@schoolsnet.act.edu.au>
2026-05-18 09:51:19 +1000

    Fixed minitask bug
    fixed issue where on interacting with GUI minitask another 2d player would popup on screen

--- gameMechanics/hacking_minitask.gd
+++ gameMechanics/hacking_minitask.gd
@@ -1,11 +1,17 @@
 extends Node2D
 
+@onready var player_scene = preload("res://gameMechanics/2d_player.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	spawn_player()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func spawn_player():
+	var new_player = player_scene.instantiate()
+	new_player.global_position = $SpawnPoint.global_position
+	add_child(new_player)
