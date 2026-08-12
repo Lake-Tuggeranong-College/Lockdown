908ca039eac7b836e72abfc3610beaabe33e1a25
Meltedvulture <0532946@schoolsnet.act.edu.au>
2026-08-12 14:05:07 +1000

    Item collection improvements

--- Scripts/ItemCollectionVolume.gd
+++ Scripts/ItemCollectionVolume.gd
@@ -14,25 +14,19 @@ func _process(delta: float) -> void:
 
 
 func _on_area_3d_body_entered(body: Node3D) -> void:
-	var temp = area3D.get_overlapping_bodies()
-	print(temp)
 	if body.has_method("getPrice"):
 		if body.getPrice() > 0:
 			itemCount += 1
 			Global.totalValue += body.getPrice()
 			if Global.myCurrentTeam == "Robber":
 				Global.updatePoints()
-			print(itemCount)
-			print(get_tree().current_scene.totalItems)
 			if get_tree().current_scene.totalItems == itemCount:
 				get_tree().current_scene.totalItems = 0
 				itemCount = 0
 				get_tree().current_scene.resetRound()
 				var bodies = area3D.get_overlapping_bodies()
 				for i in bodies:
-					print(bodies)
-					if i.getPrice() > 0:
-						i.queue_free()
+					i.queue_free()
 
 
 func _on_area_3d_body_exited(body: Node3D) -> void:
